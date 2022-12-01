ad82f65b5605b1717860804039fd297a221cbd64
digitaldweller <zvookbox@gmail.com>
2022-12-01 20:00:33 +0700

    CBRretriever test

--- tests/Integration/CBRRetrieverTest.php
+++ tests/Integration/CBRRetrieverTest.php
@@ -5,19 +5,20 @@ declare(strict_types=1);
 namespace App\Tests\Integration;
 
 use App\DTO\CurrencyRateDTO;
+use App\Retriever\CBRRetriever;
 use App\Retriever\CurrencyRatesRetrieverInterface;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-final class RatesRetrieverTest extends WebTestCase
+final class CBRRetrieverTest extends WebTestCase
 {
     /**
      * @group Integration
      */
-    public function test()
+    public function test_retrieve()
     {
         /** @var CurrencyRatesRetrieverInterface $retriever */
-        $retriever = static::getContainer()->get(CurrencyRatesRetrieverInterface::class);
+        $retriever = static::getContainer()->get(CBRRetriever::class);
         $result = $retriever->retrieve(new DateTime());
         foreach ($result as $item) {
             self::assertInstanceOf(CurrencyRateDTO::class, $item);
